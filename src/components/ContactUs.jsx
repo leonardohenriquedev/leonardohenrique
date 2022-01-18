6b3c221ae2ee03377ab642d636aaa9c763cbb2d8
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2022-01-17 21:45:20 -0300

    css do formulario de contato

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import './styles/contact.css';
 
 export const ContactUs = () => {
   const form = useRef();
@@ -28,9 +29,9 @@ export const ContactUs = () => {
   return (
     <form ref={form} onSubmit={sendEmail} className="formularioContato">
       <label>NOME</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" autoComplete="off" className='inputName'/>
       <label>EMAIL</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" autoComplete="off"  className='inputEmail' />
       <label>MENSAGEM</label>
       <textarea name="message" />
       <input type="submit" value="Enviar" className="submit" />
